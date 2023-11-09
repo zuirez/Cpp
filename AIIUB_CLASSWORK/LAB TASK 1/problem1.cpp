38053d3ba74a4e6a69e9d566122b0045a02bb11a
Rashed <140999907+zuirez@users.noreply.github.com>
2023-11-09 19:35:23 +0600

    Add files via upload

---
+++ AIIUB_CLASSWORK/LAB TASK 1/problem1.cpp
@@ -0,0 +1,34 @@
+/*
+Problem-1
+Write a c++ program to store five float numbers. Find out the summation and average of five numbers.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,c,d,e,sum,avg;
+
+    cout<<"Enter valuse of a: ";
+    cin>>a;
+
+    cout<<"Enter valuse of b: ";
+    cin>>b;
+
+    cout<<"Enter valuse of c: ";
+    cin>>c;
+
+    cout<<"Enter valuse of d: ";
+    cin>>d;
+
+    cout<<"Enter valuse of e: ";
+    cin>>e;
+
+    sum=a+b+c+d+e;
+
+    avg=sum/5;
+
+    cout<<"The summation is : "<<sum<<endl;
+    cout<<"The average is : "<<avg<<endl;
+}
---
+++ AIIUB_CLASSWORK/LAB TASK 1/problem2.cpp
@@ -0,0 +1,32 @@
+/*
+
+problem-2
+Write a c++ program to print the following information:
+
+=> Your Name
+=> Your ID
+=> Your University Name
+=> Your Department Name
+=> Your Faculty Name
+=> Semester
+=> Your Department Head Name
+=> Your Department Associate Dean Name
+=> Your Department Dean Name
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+     cout<< "My Name : MD. Rashedul Islam Shawon" << endl
+         << "My ID : 23-53193-3" << endl
+         << "My University Name : American International University - Bangladesh (AIUB)" << endl
+         << "My Department Name : Bachelor of Science in Computer Science & Engineering" << endl
+         << "My Faculty Name : Faculty of Science and Technology" << endl
+         << "My Current Semester : 1st Semester (Fall 23-24)" << endl
+         << "My Department Head Name : Dr. Akinul Islam Jony" << endl
+         << "My Department Associate Dean Name : Dr. Dip Nandi" << endl
+         << "My Department Dean Name : Mashiour Rahman" << endl;
+}
